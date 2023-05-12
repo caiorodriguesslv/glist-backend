0550b01d0ccef900cd711031fa8a92c5875949e7
Caio Rodrigues <caiorodriguesmll@gmail.com>
2023-05-12 20:55:38 -0300

    Properties, Game, ORM

---
+++ src/main/java/com/caio/GList/GListApplication.java
@@ -0,0 +1,13 @@
+package com.caio.GList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GListApplication.class, args);
+	}
+
+}
