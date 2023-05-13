3a2a740416b978d1aa81d6f9731a5c140251b172
Caio Rodrigues <caiorodriguesmll@gmail.com>
2023-05-12 21:48:57 -0300

    Game Seed

--- src/main/java/com/caio/GList/entities/Game.java
+++ src/main/java/com/caio/GList/entities/Game.java
@@ -18,23 +18,29 @@ public class Game {
 	private Long id;
 	private String title;
 	@Column(name = "game_year")
-	private Integer year;//Palavra reservada no SQL
+	private Integer year;
 	private String genre;
 	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")// Troca o VARCHAR(255) no bd.
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -80,6 +86,14 @@ public class Game {
 		this.platforms = platforms;
 	}
 
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
 	public String getImgUrl() {
 		return imgUrl;
 	}
@@ -121,8 +135,5 @@ public class Game {
 		Game other = (Game) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
 
 }
