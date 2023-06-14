2e566fb5904e60dc11da22409c2d3c5a3363900c
Caio Rodrigues <caiorodriguesmll@gmail.com>
2023-06-14 17:36:15 -0300

    GET Game by id

--- src/main/java/com/caio/GList/services/GameService.java
+++ src/main/java/com/caio/GList/services/GameService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.caio.GList.dto.GameDTO;
 import com.caio.GList.dto.GameMinDTO;
 import com.caio.GList.entities.Game;
 import com.caio.GList.repositories.GameRepository;
+
+
 
 @Service
 public class GameService {
@@ -15,6 +19,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
